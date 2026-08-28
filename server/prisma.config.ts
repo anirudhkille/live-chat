659c4c108a845a3cfa69f90db9d80d3070a348ed
anirudhkille <anirudhkille@gmail.com>
2026-08-28 21:55:01 +0530

    chages prisma config

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-   url: env("DATABASE_URL"),
+    url:
+      process.env.DATABASE_URL ??
+      "postgresql://dummy:dummy@localhost:5432/dummy",
   },
 });
